d2d398e74440c2d2cb386d6f7cf29339a478f7e7
ItSUMIT-789 <sumitshidole789@gmail.com>
2026-06-03 10:46:46 +0530

    added test cases

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,12 @@
 import pytest
 from calculator import add
+from calculator import sub
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+    assert add(0, 0) == 0
     
+def test_sub():
+    assert sub(5, 2) == 3
+    assert sub(0, 1) == -1  
+    assert sub(10, 5) == 5
